4cae3c7f113ef35884fedcc8cbe2571fb1b43b30
nathanH <roundingle@gmail.com>
2019-04-22 12:47:45 +0200

    changes

--- core/routes/auth.js
+++ core/routes/auth.js
@@ -12,7 +12,6 @@ router.get('/login', (req, res, next) => {
 });
 
 router.post(
-<<<<<<< HEAD
     '/login',
     passport.authenticate('local', {
         successRedirect: '/',
@@ -23,38 +22,15 @@ router.post(
 );
 
 router.get('/signup', (req, res, next) => {
-    res.render('auth/signup');
+    res.render('auth/signup', { layout: 'welcome-layout' });
 });
-=======
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/auth/login",
-    failureFlash: true,
-    passReqToCallback: true
-  })
-)
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup", { layout: "welcome-layout" })
-})
-
-router.post("/signup", (req, res, next) => {
-  const { firstName, lastName, email, username, password } = req.body.username
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" })
-    return
-  }
->>>>>>> origin/development
 
 router.post('/signup', (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const { firstName, lastName, email, username, password } = req.body;
     if (username === '' || password === '') {
         res.render('auth/signup', { message: 'Indicate username and password' });
         return;
     }
-
     User.findOne({ username }, 'username', (err, user) => {
         if (user !== null) {
             res.render('auth/signup', { message: 'The username already exists' });
