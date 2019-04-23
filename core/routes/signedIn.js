5884fec8ba93489962dab41224fe090dc766d70a
nathanH <roundingle@gmail.com>
2019-04-23 10:24:45 +0200

    incorporated changes from Antonis dev branch

--- core/routes/signedIn.js
+++ core/routes/signedIn.js
@@ -1,61 +1,25 @@
 // Setting required modules
-<<<<<<< HEAD
-const express = require("express")
-const router = express.Router()
-const User = require("../models/User")
-=======
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const User = require('../models/User');
 const Project = require('../models/Project');
 const Task = require('../models/Task');
->>>>>>> dashboard
 
 // Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt")
-const bcryptSalt = 10
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
 
 // authentication middleware
 const authenticationCheck = (req, res, next) => {
-<<<<<<< HEAD
-  console.log("maybe here")
-  if (req.isAuthenticated()) next()
-  else res.render("error", { errorMessage: "You need to Login to access this page" })
-}
-=======
     if (req.isAuthenticated()) next();
     else res.render('error', { errorMessage: 'You need to Login to access this page' });
 };
->>>>>>> dashboard
 
-router.get("/dashboard", authenticationCheck, (req, res, next) => {
-  res.render("signedIn/dashboard")
-})
+router.get('/dashboard', authenticationCheck, (req, res, next) => {
+    res.render('signedIn/dashboard');
+});
 
-<<<<<<< HEAD
-router.get("/addProject", authenticationCheck, (req, res, next) => {
-  res.render("signedIn/add-project")
-})
-
-router.post("/rooms/add", authenticationCheck, (req, res, next) => {
-  const { projectName, task, assignedUser, description, weight } = req.body
-  console.log(req.user._id)
-  Project.create({ projectName, task, assignedUser, description, weight, owner: req.user._id })
-    .then(project => {
-      res.redirect("/signedIn/dashboard")
-    })
-    .catch(err => {
-      console.error("Error while adding a new project", err)
-    })
-})
-
-// router.get("/news", (req, res, next) => {
-//   res.render("signedIn/news")
-// })
-
-module.exports = router
-=======
 router.get('/add-project', authenticationCheck, (req, res, next) => {
     res.render('signedIn/add-project');
 });
@@ -80,4 +44,3 @@ router.get('/api/project', (req, res, next) => {
 });
 
 module.exports = router;
->>>>>>> dashboard
